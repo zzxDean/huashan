9436c677ec0249e941ac56e4cf718fd7daf1f079
380214469 <380214469@qq.com>
2020-05-26 00:02:16 +0800

    111

---
+++ src/main/index.jsp
@@ -0,0 +1,15 @@
+<%@page import="com.youdian.user.HuaZai"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<%=new HuaZai().getName()  %>
+	<%=new HuaZai().getAge()  %>
+	<h1>Git自动化部署就是这么牛逼！</h1>
+</body>
+</html>
---
+++ src/main/java/com/youdian/user/HuaZai.java
@@ -0,0 +1,33 @@
+package com.youdian.user;
+
+import java.io.Serializable;
+
+public class HuaZai implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String name = "华仔";
+
+	private String age = "666";
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+	
+
+}
